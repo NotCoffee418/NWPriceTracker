9dffbceb8a152bca85e13f2af7134a97ad6bf00b
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2021-10-19 20:23:57 +0200

    Add Blazorise

--- NWPriceTracker/Client/Program.cs
+++ NWPriceTracker/Client/Program.cs
@@ -1,8 +1,13 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using NWPriceTracker.Client;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+      .AddBlazorise(options =>
+      {
+          options.ChangeTextOnKeyPress = true;
+      })
+      .AddBootstrapProviders()
+      .AddFontAwesomeIcons();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
---
+++ NWPriceTracker/Client/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using NWPriceTracker.Client;
+
+global using Blazorise;
+global using Blazorise.Bootstrap;
+global using Blazorise.Icons.FontAwesome;
