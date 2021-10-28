c05e337d2350dd80cb68eb8af84a5014f84d4cff
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2021-10-28 22:21:12 +0200

    Add 3 more zones + rearrange alphabetically

--- NWPriceTracker/Shared/Data/EmbeddedData.cs
+++ NWPriceTracker/Shared/Data/EmbeddedData.cs
@@ -3,18 +3,21 @@
     public static readonly List<ZoneInfo> Zones = new()
     {
         new(1, "BW", "Brightwood"),
+        new(18, "CP", "Cleave's Point"),
         new(2, "CK", "Cutlass Keys"),
+        new(19, "EB", "Eastburn"),
         new(3, "ER", "Ebonscale Reach"),
         new(5, "EF", "Everfall"),
         new(6, "FL", "First Light"),
+        new(16, "LS", "Last Stand"),
         new(8, "MB", "Monarchs Bluffs"),
+        new(14, "MH", "Mountainhome"),
+        new(15, "MR", "Mountainrise"),
         new(9, "MD", "Mourningdale"),
         new(10, "RW", "Reekwater"),
         new(11, "RS", "Restless Shore"),
         new(13, "WF", "Weavers Fen"),
-        new(14, "MH", "Mountainhome"),
-        new(15, "MR", "Mountainrise"),
-        new(16, "LS", "Last Stand"),
         new(17, "VH", "Valor Hold"),
+        new(20, "WW", "Windsward"),
     };
 }
